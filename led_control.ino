e24346a0fc87f162542a51a5e726da1985851caa
nclaude701 <118156351+nclaude701@users.noreply.github.com>
2024-09-25 14:53:45 +0200

    Create led_control.ino

---
+++ led_control.ino
@@ -0,0 +1,45 @@
+#define BLYNK_TEMPLATE_ID "TMPL2RdhGZqdR"
+#define BLYNK_TEMPLATE_NAME "LED CONTLOL"
+#define BLYNK_AUTH_TOKEN "Vc8fJuokIVUeYFrUgpZwLeRt0mOR_aY3"
+
+#define BLYNK_PRINT Serial
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
+
+char ssid[] = "nclaude";
+char pass[] = "ndago123";
+
+int blue = 14;
+int green = 12;
+
+BLYNK_WRITE(V0) {
+  int buttonState = param.asInt();
+  if (buttonState == 1) {
+    digitalWrite(blue, HIGH);
+  } else {
+    digitalWrite(blue, LOW);
+  }
+}
+
+BLYNK_WRITE(V1) {
+  int buttonState = param.asInt();
+  if (buttonState == 1) {
+    digitalWrite(green, HIGH);
+  } else {
+    digitalWrite(green, LOW);
+  }
+}
+
+void setup() {
+  Serial.begin(9600); // Initialize serial communication at 9600 baud
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass); // Connect to Blynk and WiFi
+
+  pinMode(blue, OUTPUT);
+  pinMode(green, OUTPUT);
+  digitalWrite(blue, LOW);
+  digitalWrite(green, LOW);
+}
+
+void loop() {
+  Blynk.run();
+}
